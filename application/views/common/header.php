2d782cb3b32560b7a2b952db95d25cb2d89e26e9
Rohit Singh <rohitsinghsuriyal@gmail.com>
2024-10-16 12:29:29 +0530

    header_img_updated

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -42,7 +42,7 @@
 	
     
 	<!-- Bootstrap CSS -->
-	<link rel="icon" href="<?php echo base_url('assets/images/school.jpg'); ?>" type="image/x-icon">
+	<link rel="icon" href="<?php echo base_url('assets/images/FINDMYSCHOOL.png'); ?>" type="image/x-icon">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
